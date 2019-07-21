3502697e0d60f30fa78824b488b18dde9d511b82
root <82651730@qq.com>
2019-07-21 18:43:53 +0800

    修改获取用户消息接口

--- car-api/src/main/java/com/car/entity/SysInfo.java
+++ car-api/src/main/java/com/car/entity/SysInfo.java
@@ -9,7 +9,6 @@ public class SysInfo implements Serializable {
 	
 	private Long id;
 	private Long userId;
-	private Long projectId;
 	private Integer infoType;
 	private Date produceDate;
 	private String produceUserName;
@@ -33,14 +32,6 @@ public class SysInfo implements Serializable {
 
 	public void setUserId(Long userId) {
 		this.userId = userId;
-	}
-
-	public Long getProjectId() {
-		return projectId;
-	}
-
-	public void setProjectId(Long projectId) {
-		this.projectId = projectId;
 	}
 
 	public Integer getInfoType() {
