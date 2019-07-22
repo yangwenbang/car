d20ce7775a99169d7f00f5af17a6bd9740fa7919
yangwenbang <1471275556@qq.com>
2019-07-22 19:32:40 +0800

    xxx

--- car-admin/src/main/java/com/car/test.java
+++
@@ -1,9 +0,0 @@
-package com.car;
-
-import com.car.modules.sys.shiro.ShiroUtils;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println(ShiroUtils.sha256("admin123", "YzcmCZNvbXocrsz9dm8e"));
-    }
-}
